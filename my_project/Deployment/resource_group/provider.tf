06863c8bfd4c8ed09ded664ebcc928d6230805f6
pawan <pawan@brijkishorgupta290gmail.onmicrosoft.com>
2025-12-11 00:36:56 +0530

    fix_1

--- my_project/Deployment/resource_group/provider.tf
+++ my_project/Deployment/resource_group/provider.tf
@@ -1,4 +1,43 @@
+terraform {
+  required_version = ">= 1.2.2"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.79.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~>2.45.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "Bestel"
+    storage_account_name = "bestelbackend"
+    container_name       = "bestelbackend"
+    key                 = "basic.tfstate"
+    subscription_id     = "__backend_subscription_id__"
+    tenant_id           = "__tenant_id__"
+    client_id           = "__backend_client_id__"
+    client_secret       = "__backend_client_secret__"
+    use_azuread_auth    = true
+  }
+}
+
 provider "azurerm" {
   features {}
-  subscription_id = "b4461466-1e6b-4be2-bb70-1e96a72a41c8"
+  subscription_id = "__subscription_id__"
+  tenant_id       = "__tenant_id__"
+  client_id       = "__client_id__"
+  client_secret   = "__client_secret__"
+}
+
+provider "azuread" {
+  tenant_id     = "__tenant_id__"
+  client_id     = "__client_id__"
+  client_secret = "__client_secret__"
 }
