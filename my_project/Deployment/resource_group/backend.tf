67c817016f1295a5d4fa3915a7008e51236473a3
pawan <pawan@brijkishorgupta290gmail.onmicrosoft.com>
2025-12-11 00:37:50 +0530

    b

--- my_project/Deployment/resource_group/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Bestel"
-    storage_account_name = "bestelbackend"
-    container_name       = "bestelbackend"
-    key                  = "basic.tfstate"
-  }
-}
